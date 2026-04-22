fdabc6c7157e13cda8b86ce6da408096b5e8b440
jayp090 <w2105573@westminster.ac.uk>
2026-04-22 12:48:23 +0100

    Refactor accounts app: consolidate URL and view definitions into appropriate files

--- OpenSky/accounts/views.py
+++ OpenSky/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Home page")
+
+
+def user_login(request):
+    return HttpResponse("User login page")
+
+
+def admin_login(request):
+    return HttpResponse("Admin login page")
+
+
+def signup(request):
+    return HttpResponse("Signup page")
+
+
+def dashboard(request):
+    return HttpResponse("User dashboard")
+
+
+def admin_dashboard(request):
+    return HttpResponse("Admin dashboard")
