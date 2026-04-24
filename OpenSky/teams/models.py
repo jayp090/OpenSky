1236e9949be42866494cf1dc2301c02efcfc773f
jayp090 <w2105573@westminster.ac.uk>
2026-04-24 13:56:28 +0100

    Fix docstring for Department model to improve clarity

--- OpenSky/teams/models.py
+++ OpenSky/teams/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Department(models.Model):
-    """A Sky engineering department (e.g. Platform Engineering)."""
+    """Represents a Sky engineering department (e.g. Platform Engineering)."""
     name = models.CharField(max_length=100)
     head = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
